c51205c49f6d6d236be6895f275d7833d99ed3a5
kaniGupta <kanishkgupta13@gmail.com>
2021-02-15 15:53:16 +0530

    added like button functionality.

--- scripts/post.js
+++ scripts/post.js
@@ -1,10 +1,22 @@
+let likeCount = 0;
+
 const loadData = () => {
   const username = sessionStorage.getItem('username');
   const title = sessionStorage.getItem('title');
   const summary = sessionStorage.getItem('summary');
+
   document.getElementById('single_post_username').innerHTML = username;
   document.getElementById('single_post_heading').innerHTML = title;
   document.getElementById('single_post_summary').innerHTML = summary;
+  document.getElementById('like_status').innerHTML =
+    'Be the first one to like this!';
+};
+
+const onLikeBtnClicked = () => {
+  likeCount++;
+  document.getElementById(
+    'like_status'
+  ).innerHTML = `${likeCount} person likes this!`;
 };
 
 loadData();
